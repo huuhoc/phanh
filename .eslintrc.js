55eeb358e2e113666057f35b5b93f503b4b8fcac
Huu Hoc <hocph@thinkpro.vn>
2020-07-20 09:31:19 +0700

    u

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+}
